a3e2d2000824ca67603f5fa4b08c226dba68ad79
DT1003 <deepthic1003@gmail.com>
2023-06-03 11:12:56 -0500

    "SecondCommit:Written basic java code"

---
+++ src/main/java/com.example/Intro.java
@@ -0,0 +1,8 @@
+package com.example;
+public class Intro{
+    public static void main(String args[]){
+
+        System.out.println("This is used for testing the execution of java program");
+
+    }
+}
